76c7d7885ae4554adf40bce7f3cd3215dac5e927
Saiyashwanth7 <saiyashwanthdasari@gmail.com>
2026-06-23 15:11:38 +0530

    URL updated to land on health check

--- fuel_route_project/fuel_route/urls.py
+++ fuel_route_project/fuel_route/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from route.views import health_check
 
 urlpatterns = [
-    path("health/", health_check),
+    path("", health_check),
     path("api/", include("route.urls")),
 ]
